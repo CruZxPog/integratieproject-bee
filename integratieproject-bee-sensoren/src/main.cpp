8734b56eea59002cda35050e357757250ee0c69e
Senne <sennevandesype1@gmail.com>
2025-05-16 11:30:28 +0200

    Update main.cpp voor raspberry piV1

--- integratieproject-bee-sensoren/src/main.cpp
+++ integratieproject-bee-sensoren/src/main.cpp
@@ -6,8 +6,8 @@
 #include <Adafruit_SHT4x.h>
 
 // DHT22 Setup
-#define DHTPIN1 2
-#define DHTPIN2 3 
+#define DHTPIN1 11
+#define DHTPIN2 4 
 #define DHTTYPE DHT22 
 DHT_Unified dht1(DHTPIN1, DHTTYPE);
 DHT_Unified dht2(DHTPIN2, DHTTYPE);
@@ -42,65 +42,31 @@ void setup() {
 }
 
 void loop() {
-  Serial.println("---- Sensor Readings ----");
-
-  // Read DHT22 Sensor 1 Temperature and Humidity
+  // DHT22 Sensor 1
   sensors_event_t dhtEvent;
   dht1.temperature().getEvent(&dhtEvent);
-  if (isnan(dhtEvent.temperature)) {
-    Serial.println("Error reading DHT22 Sensor 1 temperature!");
-  } else {
-    Serial.print("DHT22 Sensor 1 Temperature: ");
-    Serial.print(dhtEvent.temperature);
-    Serial.println(" °C");
-  }
-
+  float dht1Temp = dhtEvent.temperature;
   dht1.humidity().getEvent(&dhtEvent);
-  if (isnan(dhtEvent.relative_humidity)) {
-    Serial.println("Error reading DHT22 Sensor 1 humidity!");
-  } else {
-    Serial.print("DHT22 Sensor 1 Humidity: ");
-    Serial.print(dhtEvent.relative_humidity);
-    Serial.println(" %");
-  }
+  float dht1Hum = dhtEvent.relative_humidity;
 
-  // Read DHT22 Sensor 2 Temperature and Humidity
+  // DHT22 Sensor 2
   dht2.temperature().getEvent(&dhtEvent);
-  if (isnan(dhtEvent.temperature)) {
-    Serial.println("Error reading DHT22 Sensor 2 temperature!");
-  } else {
-    Serial.print("DHT22 Sensor 2 Temperature: ");
-    Serial.print(dhtEvent.temperature);
-    Serial.println(" °C");
-  }
-
+  float dht2Temp = dhtEvent.temperature;
   dht2.humidity().getEvent(&dhtEvent);
-  if (isnan(dhtEvent.relative_humidity)) {
-    Serial.println("Error reading DHT22 Sensor 2 humidity!");
-  } else {
-    Serial.print("DHT22 Sensor 2 Humidity: ");
-    Serial.print(dhtEvent.relative_humidity);
-    Serial.println(" %");
-  }
+  float dht2Hum = dhtEvent.relative_humidity;
 
-  // Read SHT4x Temperature and Humidity
+  // SHT4x
   sensors_event_t shtHumidity, shtTemp;
   uint32_t timestamp = millis();
   sht4.getEvent(&shtHumidity, &shtTemp);
   timestamp = millis() - timestamp;
 
-  Serial.print("SHT4x Temperature: ");
-  Serial.print(shtTemp.temperature);
-  Serial.println(" °C");
-
-  Serial.print("SHT4x Humidity: ");
-  Serial.print(shtHumidity.relative_humidity);
-  Serial.println(" %");
-
-  Serial.print("SHT4x Read Time: ");
-  Serial.print(timestamp);
-  Serial.println(" ms");
-
-  Serial.println("--------------------------\n");
-  delay(3000); 
+  // Print alles op één regel, gescheiden door komma's
+  Serial.print("DHT1 "); Serial.print(dht1Temp); Serial.print("°C, ");
+  Serial.print("DHT1 "); Serial.print(dht1Hum); Serial.print("%, ");
+  Serial.print("DHT2 "); Serial.print(dht2Temp); Serial.print("°C, ");
+  Serial.print("DHT2 "); Serial.print(dht2Hum); Serial.print("%, ");
+  Serial.print("SHT "); Serial.print(shtTemp.temperature); Serial.print("°C, ");
+  Serial.print("SHT "); Serial.print(shtHumidity.relative_humidity); Serial.print("%, ");
+  delay(3000);
 }
